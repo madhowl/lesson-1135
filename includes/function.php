c69815ee1b9afc3ebc097aa5f39a69b9dfcf8a1a
MadHowl <madhowl@yandex.ru>
2021-10-05 08:38:06 +1000

    Auto stash before merge of "master" and "origin/master"

--- includes/function.php
+++ includes/function.php
@@ -1,4 +1,24 @@
 <?php
+function dd($some){
+    echo '<pre>';
+    print_r($some);
+    echo '</pre>';
+
+};
+function getArticleList(){
+    $dir    = 'content/pages/';
+    $filesList = scandir($dir);
+    dd($filesList);
+    $pages = glob($dir . "*.md");
+    dd($pages);
+    foreach($pages as $page) {
+        $pageName = substr($page, 8);
+        $pageName = substr($pageName, 0, -3);
+        echo "<li><a href=\"index.php?id=".$pageName."\">".$pageName."</a></li>";
+
+    }
+
+};
 function main(){
     if (!isset($_REQUEST['page'])){
         include ('content/pages/main.md');
@@ -7,6 +27,7 @@ function main(){
         switch ($page){
             case 'about': include ('content/pages/about.md');break;
             case 'calc': include ('content/pages/calc.php');break;
+            case 'article': getArticleList();break;
 
             default: include ('content/pages/404.md');
         }
@@ -20,6 +41,7 @@ function articleList(){
         dbg($page);
     }
 }
+
 
 function calc(){
     if(isset($_POST['btnCalc'])){
